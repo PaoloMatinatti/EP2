1c574d451d2a83526cda42c78add00ea0e55af1e
riadaltinawi <81169967+riadaltinawi@users.noreply.github.com>
2021-04-27 16:55:25 -0300

    possui_movimentos_possiveis ERRO 1

---
+++ possui_movimentos_possiveis.py
@@ -0,0 +1,39 @@
+def possui_movimentos_possiveis(movimento):
+    import random
+def cria_baralho ():
+    baralho= []
+    cartas= ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    c=0
+    c2=0
+    c3=0
+    c4=0
+    i=0
+    caralos = True
+    while caralos:
+        if c <= 13 -1:
+            paus= cartas[c] + '♣'
+            baralho.append(paus)
+            c+=1
+            i+=1
+        if c2 <=13-1:
+            espadas= cartas[c2]+ '♠'
+            baralho.append(espadas)
+            c2+=1
+            i+=1
+        if c3 <=13-1:
+           copas= cartas[c3]+ '♥'
+           baralho.append(copas)
+           c3+=1
+           i+=1
+        if c4 <=13-1:
+            ouros= cartas[c4]+ '♦'
+            baralho.append(ouros)
+            c4+=1
+            i+=1
+        else:
+            caralos = False
+    random.shuffle(baralho)
+    return baralho
+    if cartas=='10':
+        if [2]=='♣' or [2]=='♠'and '♣' not in cartas:
+            return False
