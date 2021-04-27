6df2c8cb21b9f046c6655415fa2a3ba55eed0f4c
Paolo Matinatti <paolomatinatti@gmail.com>
2021-04-26 22:47:03 -0300

    final

--- Lista movimentos.py
+++ Lista movimentos.py
@@ -1,25 +1,30 @@
 def lista_movimentos_possiveis (cartas,indice):
     lugar = cartas[indice]
     movimento=[]
- #verifica numero e naipe pra 10  
-    if lugar[0] + lugar [1] == '10' :
-        if lugar[1] == cartas[indice-1][1] or lugar[2] == cartas[indice-1][1]:
-                movimento.append(1)
-        if lugar[1] == cartas[indice-3][1] or lugar[2] == cartas[indice-3][1] :
-                movimento.append(3)
-        return movimento
- #verifica o numero
     if indice == 0:
         return movimento
-    elif lugar[0] == cartas[indice-1][0] or lugar[1] ==cartas[indice-1][1] or lugar[1] == cartas[indice-1][2] :
+    elif lugar[0] == cartas[indice-1][0] or lugar[1] ==cartas[indice-1][1] :
         movimento.append(1)
- #verifca o naipe
+    elif len(cartas[indice-1]) == 3:
+        if lugar[1] == cartas[indice-1][2]:
+            movimento.append(1)
+    elif len(lugar) == 3:
+        if lugar[2] == cartas[indice-1][1]:
+            movimento.append(1)
+
     if indice ==1 or indice ==2:
         return movimento
-    elif lugar[0] == cartas[indice-3][0] or lugar[1] == cartas[indice-3][1] or lugar[1] == cartas[indice-3][2]:
+    elif lugar[0] == cartas[indice-3] or lugar[1] == cartas[indice-3][1]:
         movimento.append(3)
+        print(cartas[indice-3])
+    elif len(cartas[indice-3]) == 3:
+        if lugar[1] == cartas[indice-3][2]:
+            movimento.append(3)
+    elif len(lugar) == 3:
+        if lugar[2] == cartas[indice-3][1]:
+            movimento.append(3)
+        
 
-    
+    return movimento
 
-    return movimento  
-print(lista_movimentos_possiveis(['10♦', '10♥', '10♣', '10♠', '10♣'],4))
+print(lista_movimentos_possiveis(['A♠', 'j♠', 'Q♠', 'k♠', '10♠'],4))
