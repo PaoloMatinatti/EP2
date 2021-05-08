f87b2a1596032d7034412c3b43d796cf5bef245a
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-08 20:29:23 -0300

    .

--- mainteste.py
+++ mainteste.py
@@ -2,8 +2,11 @@ import baralho
 import Lista_movimentos
 import possui_movimentos_possiveis_final
 baralho= baralho.cria_baralho()
-print(baralho)
-print(Lista_movimentos.lista_movimentos_possiveis(baralho,51))
+c=1
+for item in baralho:
+    print('{0}. {1}'.format(c,item))
+    c+=1
+
 valor=int(input('Escolha uma carta (digite um número entre 1 e 52): '))
 carta=baralho[valor-1]
 if valor >= 53:
