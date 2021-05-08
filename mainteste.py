7d32260ad56ee35e8c10d1cbfc196a55d9cea6dc
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-08 20:46:21 -0300

    .

--- mainteste.py
+++ mainteste.py
@@ -6,12 +6,13 @@ c=1
 for item in baralho:
     print('{0}. {1}'.format(c,item))
     c+=1
-
-valor=int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
-if valor >= 53:
-    print('carta invalida')
-else:
-    carta=baralho[valor-1]
-    print(carta)
-
-
+c=1
+test=True
+while test:
+    valor=int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
+    if valor >= 53:
+        print('carta invalida')
+    else:
+        carta=baralho[valor-1]
+        print(carta)
+    
