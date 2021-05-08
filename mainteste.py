db7b8576c60bb38f1c84feae514f098fd023bd3e
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-08 19:29:06 -0300

    escolhe carta e verifica se é valida

--- mainteste.py
+++ mainteste.py
@@ -4,7 +4,8 @@ import possui_movimentos_possiveis_final
 baralho= baralho.cria_baralho()
 print(baralho)
 print(Lista_movimentos.lista_movimentos_possiveis(baralho,51))
-if possui_movimentos_possiveis_final.possui_movimentos_possiveis(baralho,51)== True:
-    print('teste')
-else:
-    print('teste2')
+valor=int(input('Escolha uma carta (digite um número entre 1 e 52): '))
+carta=baralho[valor-1]
+if valor >= 53:
+    print('carta invalida')
+print(carta)
