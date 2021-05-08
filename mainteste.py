383d2c8c22bdbc0cb9f2a381003b4f20bf1bb19f
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-07 22:40:35 -0300

    arrumando funcoes

--- mainteste.py
+++ mainteste.py
@@ -1,6 +1,10 @@
 import baralho
 import Lista_movimentos
-
+import possui_movimentos_possiveis_final
 baralho= baralho.cria_baralho()
 print(baralho)
 print(Lista_movimentos.lista_movimentos_possiveis(baralho,51))
+if possui_movimentos_possiveis_final.possui_movimentos_possiveis(baralho,51)== True:
+    print('teste')
+else:
+    print('teste2')
