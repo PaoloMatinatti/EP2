fd470ba68ee96e3b86298dda5bd5e6472f8a5113
riadaltinawi <81169967+riadaltinawi@users.noreply.github.com>
2021-05-08 14:58:34 -0300

    tentativa fracassada

--- mainteste.py
+++ mainteste.py
@@ -1,6 +1,14 @@
 import baralho
 import Lista_movimentos
+import possui_movimentos_possiveis_final
+
 
 baralho= baralho.cria_baralho()
 print(baralho)
 print(Lista_movimentos.lista_movimentos_possiveis(baralho,51))
+carta=int(input('Escolha uma carta (digite um número entre 1 e 52): '))
+carta=carta.possui_movimentos_possiveis_final
+if carta not in possui_movimentos_possiveis_final():
+    print('carta invalida, digite outro valor: ')
+
+    
