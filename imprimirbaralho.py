d226d9cfc084c28b93c0c26ebc88eb46bf767a0c
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-08 21:40:43 -0300

    movimentos

---
+++ imprimirbaralho.py
@@ -0,0 +1,6 @@
+def imprimir(x):
+    c=1
+    for item in x:
+        print('{0}. {1}'.format(c,item))
+        c+=1
+ 
--- mainteste.py
+++ mainteste.py
@@ -1,18 +1,38 @@
 import baralho
 import Lista_movimentos
 import possui_movimentos_possiveis_final
+import Empilha
+import imprimirbaralho
 baralho= baralho.cria_baralho()
-c=1
-for item in baralho:
-    print('{0}. {1}'.format(c,item))
-    c+=1
-c=1
+
 test=True
+print(imprimirbaralho.imprimir(baralho))
 while test:
+    c=1
     valor=int(input('Escolha uma carta (digite um número entre 1 e {}): '.format(len(baralho))))
-    if valor >= 53:
+    if valor > len(baralho)  :
         print('carta invalida')
     else:
         carta=baralho[valor-1]
-        print(carta)
+    if possui_movimentos_possiveis_final.possui_movimentos_possiveis(baralho,valor) == True:
+        if Lista_movimentos.lista_movimentos_possiveis(baralho,valor) == [1] :
+            Empilha.empilha(baralho,valor,(valor-1))
+ 
+        elif Lista_movimentos.lista_movimentos_possiveis(baralho,valor) ==[3]:
+            Empilha.empilha(baralho,valor,(valor-3))
+            imprimirbaralho.imprimir(baralho)
+
+        elif Lista_movimentos.lista_movimentos_possiveis(baralho,valor) == [1,3]:
+            esc=int(input('qual movimento deseja fazer:'))
+            print('1.{}'.format(baralho[valor-1]))
+            print('2. {}'.format(baralho[valor-3]))
+            if esc == 1:
+                Empilha.empilha(baralho,valor,(valor-1))
+            else:
+                 Empilha.empilha(baralho,valor,(valor-3))
+    elif possui_movimentos_possiveis_final.possui_movimentos_possiveis(baralho,valor)== False:
+        print('movimento invalido')
+           
+
+            
     
