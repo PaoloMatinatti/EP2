7bf2cb164d85d50a673fb851a5a4b4b1097500a6
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-11 20:38:13 -0300

    jogar novamente

--- ganhououperdeu.py
+++ ganhououperdeu.py
@@ -2,8 +2,20 @@ def ganhou_ou_perdeu(baralho):
     if len(baralho) > 1:
         a='perdeu'
         print("Você \033[031m{}\033[m".format(a))
-        print(" ")
+        print(" ")     
     else:
         b='Ganhou'
         print("Você \033[32m{}\033[m".format(b))
         print(" ")
+    tt= True
+    while tt:
+        fim = input('deseja jogar novamente? (digite s ou  n): ')
+        if fim == 'n' :
+            return False
+            tt= False
+        elif fim == 's' :
+            return True
+            tt= False
+        elif fim != 'n' and fim != 's' :
+            print('movimento invalido')
+   
--- jogo_final.py
+++ jogo_final.py
@@ -7,16 +7,17 @@ inicio = input("aperte ENTER para iniciar o jogo...")
 
 
 fim_do_jogo = True
-baralho = baralho.cria_baralho()
+baralhos = baralho.cria_baralho()
 
 while fim_do_jogo:
 
-    baralho = movimentos.movimentos(baralho)
-    fim_do_jogo = ganhououperdeu.ganhou_ou_perdeu(baralho)
+    baralhos = movimentos.movimentos(baralhos)
+    acabou = ganhououperdeu.ganhou_ou_perdeu(baralhos)
     
-    if fim_do_jogo == "s":
-        fim_do_jogo == True
-        baralho = baralho.cria_baralho()
+    if acabou == False:
+        fim_do_jogo= False
         
-    elif fim_do_jogo == "n":
-        fim_do_jogo == False
+    else:
+        baralhos = baralho.cria_baralho()
+
+print ('Obrigado por jogar')
