abe188cbe95c8ffad7da8556f3ac097f2b4e4420
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-11 20:53:51 -0300

    Baralho arrumado

--- baralho.py
+++ baralho.py
@@ -7,16 +7,30 @@ def cria_baralho ():
     c3=0
     c4=0
     i=0
-    caralos = True
-    while caralos:
+    card = True
+    while card:
         if c <= 13 -1:
             paus= cartas[c] + '♣'
             baralho.append('\033[35m{}\033[m'.format(paus))
             c+=1
             i+=1
-
+        if c2 <=13-1:
+            espadas= cartas[c2]+ '♠'
+            baralho.append('\033[34m{}\033[m'.format(espadas))
+            c2+=1
+            i+=1
+        if c3 <=13-1:
+           copas= cartas[c3]+ '♥'
+           baralho.append('\033[31m{}\033[m'.format(copas))
+           c3+=1
+           i+=1
+        if c4 <=13-1:
+            ouros= cartas[c4]+ '♦'
+            baralho.append('\033[33m{}\033[m'.format(ouros))
+            c4+=1
+            i+=1
         else:
-            caralos = False
+            card = False
     random.shuffle(baralho)
     return baralho
 
