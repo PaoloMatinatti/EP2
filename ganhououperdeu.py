9a36c19dc869b3c47971e2dc22b39b49d74370ac
riadaltinawi <81169967+riadaltinawi@users.noreply.github.com>
2021-05-11 19:46:24 -0300

    arrumando cor

--- ganhououperdeu.py
+++ ganhououperdeu.py
@@ -1,13 +1,13 @@
 def ganhou_ou_perdeu(baralho):
     if baralho > 1:
         a='perdeu'
-        print("Você \033[031m{}".format(a))
+        print("Você \033[031m{}\033[m".format(a))
         print(" ")
         jogar_de_novo = str(input("Quer de jogar de novo? (digite S ou N): "))
         return jogar_de_novo
     else:
         b='Ganhou'
-        print("Você \033[32m{}".format(b))
+        print("Você \033[32m{}\033[m".format(b))
         print(" ")
         jogar_de_novo = str(input("Quer de jogar de novo? (digite S ou N): "))
         return jogar_de_novo
