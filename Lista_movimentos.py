ae653c55f69aed1bb6e9ca1bfcb3da4f875bc1b5
Paolo Matinatti <paolomatinatti@gmail.com>
2021-05-11 19:49:06 -0300

    teste

--- Lista_movimentos.py
+++ Lista_movimentos.py
@@ -2,28 +2,29 @@ import extrair_naipe
 import extrair_valor
 
 def lista_movimentos_possiveis (cartas,indice):
-    valor= extrair_valor.extrai_valor(cartas[indice])
-    naipe= extrair_naipe.extrai_naipe(cartas[indice])
-    valorant= extrair_valor.extrai_valor(cartas[indice-1])
-    naipeant= extrair_naipe.extrai_naipe(cartas[indice-1])
-    valorterc= extrair_valor.extrai_valor(cartas[indice-3])
-    naipeterc= extrair_naipe.extrai_naipe(cartas[indice-3])
     movimento=[]
     if indice == 0:
         return movimento
-    elif indice < 3:
-        if valor == valorant or naipe == naipeant:
-            movimento.append(1)
-            return movimento
     else:
-        if (valor == valorant or naipe == naipeant) and (valor == valorterc or naipe == naipeterc):
-            movimento.append(1)
-            movimento.append(3)
-            return movimento
-        elif valor == valorant or naipe == naipeant:
-            movimento.append(1)
-        elif valor == valorterc or naipe == naipeterc:
-            movimento.append(3)
-            return movimento
+        valor= extrair_valor.extrai_valor(cartas[indice])
+        naipe= extrair_naipe.extrai_naipe(cartas[indice])
+        valorant= extrair_valor.extrai_valor(cartas[indice-1])
+        naipeant= extrair_naipe.extrai_naipe(cartas[indice-1])
+        valorterc= extrair_valor.extrai_valor(cartas[indice-3])
+        naipeterc= extrair_naipe.extrai_naipe(cartas[indice-3])
+        if indice < 3:
+            if valor == valorant or naipe == naipeant:
+                movimento.append(1)
+                return movimento
+        else:
+            if (valor == valorant or naipe == naipeant) and (valor == valorterc or naipe == naipeterc):
+                movimento.append(1)
+                movimento.append(3)
+                return movimento
+            elif valor == valorant or naipe == naipeant:
+                movimento.append(1)
+            elif valor == valorterc or naipe == naipeterc:
+                movimento.append(3)
+                return movimento
     return movimento
 
--- ganhououperdeu.py
+++ ganhououperdeu.py
@@ -1,13 +1,9 @@
 def ganhou_ou_perdeu(baralho):
-    if baralho > 1:
+    if len(baralho) > 1:
         a='perdeu'
         print("Você \033[031m{}".format(a))
         print(" ")
-        jogar_de_novo = str(input("Quer de jogar de novo? (digite S ou N): "))
-        return jogar_de_novo
     else:
         b='Ganhou'
         print("Você \033[32m{}".format(b))
         print(" ")
-        jogar_de_novo = str(input("Quer de jogar de novo? (digite S ou N): "))
-        return jogar_de_novo
